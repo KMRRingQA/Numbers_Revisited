99e2af938920f7eb5e709f58af3dce2c409a61f2
Korbinian <kmr.ring@outlook.com>
2020-03-24 22:19:31 +0000

    fixed a bunch of errors, now the format is much better.

--- src/com/qa/main/NumbersTo999.java
+++ src/com/qa/main/NumbersTo999.java
@@ -26,11 +26,31 @@ public class NumbersTo999 {
 		}
 		else {
 			int Hunderterstelle = Num1/100;
-			int Zehnerstelle = (Num1/10)%10;
-			int Einerstelle = Num1%10;
-			String FullName;
-			FullName = EinerstelleName[Hunderterstelle] + " hundred "+ ZehnerstelleName[Zehnerstelle] + " " + EinerstelleName[Einerstelle];
-			return FullName;
+			if (Num1%100==0) {
+				String FullName;
+				FullName = EinerstelleName[Hunderterstelle] + " hundred ";
+				return FullName;
+			}
+			if (Num1%100<20) {
+				int Einerstelle = Num1%100;
+				String FullName;
+				FullName = EinerstelleName[Hunderterstelle] + " hundred and " + EinerstelleName[Einerstelle];
+				return FullName;
+			}
+			if (Num1%10==0) {
+				int Zehnerstelle = (Num1/10)%10;
+				int Einerstelle = Num1%10;
+				String FullName;
+				FullName = EinerstelleName[Hunderterstelle] + " hundred and " + ZehnerstelleName[Zehnerstelle] + " " + EinerstelleName[Einerstelle];
+				return FullName;
+			}
+			else {
+				int Zehnerstelle = (Num1/10)%10;
+				int Einerstelle = Num1%10;
+				String FullName;
+				FullName = EinerstelleName[Hunderterstelle] + " hundred and " + ZehnerstelleName[Zehnerstelle] + "-" + EinerstelleName[Einerstelle];
+				return FullName;
+			}
 		}
 	}
 }
