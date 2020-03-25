63477d9d9930ebb4c93079373c215a7dfce82d4b
Korbinian <kmr.ring@outlook.com>
2020-03-25 12:31:11 +0000

    x000-x020 condition, x020-x100 condition.

--- src/com/qa/main/NumbersTo999.java
+++ src/com/qa/main/NumbersTo999.java
@@ -1,64 +1,118 @@
 package com.qa.main;
 
 public class NumbersTo999 {
-	String nameThatNumber (int Num1) {
+	String nameThatNumber (int Num) {
+		String FullName;
 		final String[] EinerstelleName = {
 	            "", "one", "two", "three", "four", "five", "six", "seven",
 	            "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
 	            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
 	    };
 		final String[] ZehnerstelleName = {
-				"", "one", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"
+				"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"
 		};
-		if (Num1<20) {
-			return EinerstelleName[Num1];
+		if (Num<20) {
+			return EinerstelleName[Num];
 		}
-		else if (Num1>999) {
+		else if (Num>9999) {
 			System.out.println("You're supposed to enter a number n where 0<n<1000.");
 			return null;
 		}
-		else if (Num1<100) {
-			if (Num1%10==0) {
-				int Zehnerstelle = Num1/10;
-				String FullName;
+		else if (Num<100) {
+			if (Num%10==0) {
+				int Zehnerstelle = Num/10;
 				FullName = ZehnerstelleName[Zehnerstelle];
 				return FullName;
 			}
 			else {
-				int Zehnerstelle = Num1/10;
-				int Einerstelle = Num1%10;
-				String FullName;
+				int Zehnerstelle = Num/10;
+				int Einerstelle = Num%10;
 				FullName = ZehnerstelleName[Zehnerstelle] + "-" + EinerstelleName[Einerstelle];
 				return FullName;
 			}
 		}
-		else {
-			int Hunderterstelle = Num1/100;
-			if (Num1%100==0) {
-				String FullName;
+		else if (Num<1000)	{
+			int Hunderterstelle = Num/100;
+			if (Num%100==0) {
 				FullName = EinerstelleName[Hunderterstelle] + " hundred ";
 				return FullName;
 			}
-			if (Num1%100<20) {
-				int Einerstelle = Num1%100;
-				String FullName;
+			if (Num%100<20) {
+				int Einerstelle = Num%100;
 				FullName = EinerstelleName[Hunderterstelle] + " hundred and " + EinerstelleName[Einerstelle];
 				return FullName;
 			}
-			if (Num1%10==0) {
-				int Zehnerstelle = (Num1/10)%10;
-				int Einerstelle = Num1%10;
-				String FullName;
+			if (Num%10==0) {
+				int Zehnerstelle = (Num/10)%10;
+				int Einerstelle = Num%10;
 				FullName = EinerstelleName[Hunderterstelle] + " hundred and " + ZehnerstelleName[Zehnerstelle] + " " + EinerstelleName[Einerstelle];
 				return FullName;
 			}
 			else {
-				int Zehnerstelle = (Num1/10)%10;
-				int Einerstelle = Num1%10;
-				String FullName;
+				int Zehnerstelle = (Num/10)%10;
+				int Einerstelle = Num%10;
 				FullName = EinerstelleName[Hunderterstelle] + " hundred and " + ZehnerstelleName[Zehnerstelle] + "-" + EinerstelleName[Einerstelle];
 				return FullName;
 			}
+		}
+		else {
+			if (Num%1000==0) {
+				int Tausenderstelle = Num/1000;
+				FullName = EinerstelleName[Tausenderstelle] + " thousand";
+				return FullName;
+			}
+			else if (Num%1000<20) {
+				int Tausenderstelle = Num/1000;
+				int Zehnerstelle = (Num/100)%10;
+				int Einerstelle = Num%1000;
+				FullName = EinerstelleName[Tausenderstelle] + " thousand and " + ZehnerstelleName[Zehnerstelle] + EinerstelleName[Einerstelle];
+				return FullName;
+			}
+			else if (Num%1000<100) {
+				if (Num%10==0) {
+					int Tausenderstelle = Num/1000;
+					int Zehnerstelle = (Num/10)%10;
+					FullName = EinerstelleName[Tausenderstelle] + " thousand and " + ZehnerstelleName[Zehnerstelle];
+					return FullName;
+				}
+				else {
+					int Tausenderstelle = Num/1000;
+					int Zehnerstelle = (Num/10)%10;
+					int Einerstelle = Num%10;
+					FullName = EinerstelleName[Tausenderstelle] + " thousand and " + ZehnerstelleName[Zehnerstelle] + "-" + EinerstelleName[Einerstelle];
+					return FullName;
+				}
+			}
+			else {
+				if (Num%100==0) {
+					int Hunderterstelle = (Num/100)%10;
+					int Tausenderstelle = Num/1000;
+					FullName = EinerstelleName[Tausenderstelle] + " thousand " + EinerstelleName[Hunderterstelle] + " hundred";
+					return FullName;
+				}
+				else if (Num%100<20) {
+					int Einerstelle = Num%100;
+					int Hunderterstelle = (Num/100)%10;
+					int Tausenderstelle = Num/1000;
+					FullName = EinerstelleName[Tausenderstelle] + " thousand " + EinerstelleName[Hunderterstelle] + " hundred and " + EinerstelleName[Einerstelle];
+					return FullName;
+				}
+				else if (Num%10==0) {
+					int Tausenderstelle = Num/1000;
+					int Hunderterstelle = (Num/100)%10;
+					int Zehnerstelle = (Num/10)%10;
+					FullName = EinerstelleName[Tausenderstelle] + " thousand " + EinerstelleName[Hunderterstelle] + " hundred and "+ ZehnerstelleName[Zehnerstelle];
+					return FullName;
+				}
+					else {
+						int Tausenderstelle = Num/1000;
+						int Hunderterstelle = (Num/100)%10;
+						int Zehnerstelle = (Num/10)%10;
+						int Einerstelle = Num%10;
+						FullName = EinerstelleName[Tausenderstelle] + " thousand " + EinerstelleName[Hunderterstelle] + " hundred and "+ ZehnerstelleName[Zehnerstelle] + "-" + EinerstelleName[Einerstelle];
+						return FullName;
+					}
+				}
 		}
 	}
 }
